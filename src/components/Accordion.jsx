733277a7a536f21ede614033d4ea3ad76e829450
Colin Swan <colinswan.cs@gmail.com>
2023-05-17 21:55:23 +0100

    added in comments to code

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,16 +1,21 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import faqs from "../data/faqs";
 import AccordionItem from "./AccordionItem";
 
 const Accordion = () => {
+  // State to keep track of the currently active accordion item
   const [active, setActive] = useState(null);
 
+  // This function is called when an accordion header is clicked
   const handleFaqToggle = (index) => {
+    // If the clicked item is already active, we set active to null, closing the accordion
+    // Otherwise, we set active to the index of the clicked item, opening it
     setActive(active === index ? null : index);
   };
 
   return (
     <article>
+      {/* We map over the FAQs data, rendering an AccordionItem for each */}
       {faqs.map((faq) => (
         <AccordionItem
           key={faq.id}
--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,16 +1,21 @@
+import React from "react";
 import "./AccordionItem.css";
 import { FaChevronDown } from "react-icons/fa";
 
 const AccordionItem = ({ id, active, handleFaqToggle, question, answer }) => (
   <div className="accordion-item">
+    {/* When the header is clicked, we call handleFaqToggle with the id of this item */}
     <h2 className="accordion-header" onClick={() => handleFaqToggle(id)}>
+      {/* If this item is the active one, we add the "active" class to the button */}
       <button className={`accordion-button ${active === id ? "active" : ""}`}>
         {question}
       </button>
+      {/* If this item is the active one, we add the "active" class to the chevron */}
       <span className={`accordion-chevron ${active === id ? "active" : ""}`}>
         <FaChevronDown />
       </span>
     </h2>
+    {/* If this item is the active one, we add the "show" class to show the answer */}
     <div className={`accordion-collapse ${active === id ? "show" : ""}`}>
       <div className="accordion-body">{answer}</div>
     </div>
