77aa2fd946956c98e2a9fd57c79f9f8817c727c1
Colin Swan <colinswan.cs@gmail.com>
2023-07-11 16:41:31 +0100

    updated animation on accordion

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./AccordionItem.css";
 import { FaChevronDown } from "react-icons/fa";
 
-const AccordionItem = ({ id, active, handleFaqToggle, question, answer }) => (
-  <div className="accordion-item">
-    {/* When the header is clicked, we call handleFaqToggle with the id of this item */}
-    <h2 className="accordion-header" onClick={() => handleFaqToggle(id)}>
-      {/* If this item is the active one, we add the "active" class to the button */}
-      <button className={`accordion-button ${active === id ? "active" : ""}`}>
-        {question}
-      </button>
-      {/* If this item is the active one, we add the "active" class to the chevron */}
-      <span className={`accordion-chevron ${active === id ? "active" : ""}`}>
-        <FaChevronDown />
-      </span>
-    </h2>
-    {/* If this item is the active one, we add the "show" class to show the answer */}
-    <div className={`accordion-collapse ${active === id ? "show" : ""}`}>
-      <div className="accordion-body">{answer}</div>
+const AccordionItem = ({ id, active, handleFaqToggle, question, answer }) => {
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    if (active === id) {
+      contentRef.current.style.height = `${contentRef.current.scrollHeight}px`;
+    } else {
+      contentRef.current.style.height = "0px";
+    }
+  }, [active, id]);
+
+  return (
+    <div className="accordion-item">
+      <h2 className="accordion-header" onClick={() => handleFaqToggle(id)}>
+        <button className={`accordion-button ${active === id ? "active" : ""}`}>
+          {question}
+        </button>
+        <span className={`accordion-chevron ${active === id ? "active" : ""}`}>
+          <FaChevronDown />
+        </span>
+      </h2>
+      <div
+        ref={contentRef}
+        className={`accordion-collapse ${active === id ? "show" : ""}`}
+      >
+        <div className="accordion-body">{answer}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default AccordionItem;
